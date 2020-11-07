290bb90befd5db25a9ec1c7e204e45e32c92f360
AmanChaudhary1998 <chaudharyaman50@yahoo.com>
2020-11-07 10:14:36 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { Card, Switch } from 'antd';
-import { Route } from 'react-router-dom';
+import {  Switch } from 'antd';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+
+import Card from './components/cards/card';
 import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div>
+      <Router>
       <Navbar />
       <Switch>
         <Route exact path="/Card" component={Card}/>
       </Switch>
+      </Router>
     </div>
   );
 }
