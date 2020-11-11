8c6a3d864d74a8e39065d600ef4b566d2c37f5a8
AmanChaudhary1998 <chaudharyaman50@yahoo.com>
2020-11-11 12:07:05 +0530

    navbar-card

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import { Route, BrowserRouter as Router,Switch } from 'react-router-dom';
 import './App.css';
 
 import CardBSE from './components/cards/CardBSE';
+import DropdownMenu from './components/navbar/dropdown-menu';
 import Navbar from './components/navbar/navbar';
+import NavItem from './components/navbar/navbar-items';
+import { ReactComponent as CaretIcon } from '../src/components/navbar/icons/caret.svg';
 
 function App() {
   return (
-    <div>
-      <Router>
-      <Navbar >
-      <Switch>
-        <Route exact path="/CardBSE" component={CardBSE}/>
-      </Switch>
-      </Navbar>
-      </Router>
-    </div>
+    <>
+        <Router>
+          <Navbar>
+          <Switch>
+          <Route exact path="/cardbse" component={CardBSE} />
+        </Switch>
+      <NavItem  />
+      <NavItem icon={< CaretIcon />}>
+        <DropdownMenu />
+
+      </NavItem>
+    </Navbar>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/navbar/navbar-items.js
@@ -0,0 +1,20 @@
+import {React, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './navbar.styles.css';
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+  
+    return (
+      <>
+      <li className="nav-item">
+          <a href="#" className="icon-button" onClick={() => setOpen(!open)} >
+        {props.icon}
+            </a>
+
+        {open && props.children}
+      </li>
+      </>
+    );
+}
+export default NavItem;
