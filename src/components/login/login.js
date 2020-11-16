56965f073513d43a1358720a9fc02833d229e6f7
AmanChaudhary1998 <chaudharyaman50@yahoo.com>
2020-11-16 15:02:21 +0530

    login-design

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -24,41 +24,41 @@ class Login extends Component {
 	render() {
 		return(
 			<>
-				<div class="container" id="container">
-					<div class="form-container sign-up-container">
-						<form action="#" method="POST">
-							<h1>Create Account</h1>
-							<div class="social-container">
+				<div className="container" id="container">
+					<div className="form-container sign-up-container">
+						<form className="login_form" action="#" method="POST">
+							<h1 className="login-h1">Create Account</h1>
+							<div className="social-container">
+								<input type="text" name="name" placeholder="Name" />
+								<input type="email" name="email" placeholder="Email" />
+								<input type="password" name="password" placeholder="Password" />
+								<input type="text" name="mobile" placeholder="Mobile Number" />
 							</div>
-							<input type="text" name="name" placeholder="Name" />
-							<input type="email" name="email" placeholder="Email" />
-							<input type="password" name="password" placeholder="Password" />
-							<input type="text" name="mobile" placeholder="Mobile Number" />
-							<button type="submit">Sign Up</button>
+							<button className="login_button" type="submit">Sign Up</button>
 						</form>
 					</div>
-					<div class="form-container sign-in-container">
-						<form action="#" method="POST">
-							<h1>Sign in</h1>
+					<div className="form-container sign-in-container">
+						<form className="login_form" action="#" method="POST">
+							<h1 className="login-h1">Sign in</h1>
 							<div class="social-container">
 							</div>
 							<input type="email" name="email" placeholder="Email" />
 							<input type="password" name="password" placeholder="Password" />
-							<a href="#">Forgot your password?</a>
-							<button type="submit">Sign In</button>
+							<a className="login_a" href="#">Forgot your password?</a>
+							<button className="login_button" type="submit">Sign In</button>
 						</form>
 					</div>
-					<div class="overlay-container">
-						<div class="overlay">
-							<div class="overlay-panel overlay-left">
-								<h1>StoXkeeper</h1>
-								<p>Invest with us Today...</p>
-								<button class="ghost" id="signIn">Sign In</button>
+					<div className="overlay-container">
+						<div className="overlay">
+							<div className="overlay-panel overlay-left">
+								<h1 className="login-h1">StoXkeeper</h1>
+								<p className="login-p">Invest with us Today...</p>
+								<button class="ghost login_button" id="signIn">Sign In</button>
 							</div>
-							<div class="overlay-panel overlay-right">
-								<h2>Welcome Investor</h2>
-								<p>Enjoy the world of Market with StoXkeeper</p>
-								<button class="ghost" id="signUp">Sign Up</button>
+							<div className="overlay-panel overlay-right">
+								<h2 className="login-h2">Welcome Investor</h2>
+								<p className="login-p">Enjoy the world of Market with StoXkeeper</p>
+								<button className="ghost login_button" id="signUp">Sign Up</button>
 							</div>
 						</div>
 					</div>
