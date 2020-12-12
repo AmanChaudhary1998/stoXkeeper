ff6fe91ca91a3be389ebc7c66eec1491a6798ba6
Hemant Singh <singh.hemant9583@gmail.com>
2020-12-12 14:18:45 +0530

    fixes

--- src/components/home-page/heading/heading.jsx
+++ src/components/home-page/heading/heading.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import Typical from 'react-typical';
 import './heading.css';
-
+// import Mid from './mid';
+import ico from '../../../images/ico.gif';
 import graphs from "../../../images/graphs.gif";
 import hero from "../../../images/hero.svg";
 import coin from "../../../images/coin.png"
-import { NavLink } from "react-router-dom";
+// import Typo from "./Typo";
 class Heading extends React.Component {
     render () {
       return (
           <div>
-            <div>
-{/* <<<<<<< HEAD
-              <div class="logo"><img  class="coin"src={coin} alt="coin" srcset=""/><h1 class="brand">StoXkeeper</h1></div> 
-              <img class="graph"src={hero} alt="" srcset=""/> */}
-            <p class="typical"><Typical
-          steps={['Stocks analysis 📈', 800, 'Latest news 📰', 800,'Prediction🔮',800]}
-          loop={Infinity}
-          wrapper="p"
-        /></p>
+            <div className="HeadingContainer">
               {/* <div class="logo"><img  class="coin"src={coin} alt="coin" srcset=""/><h1 class="brand">StoXkeeper</h1></div>  */}
               <img class="graph"src={graphs} alt="" srcset=""/>
-{/* >>>>>>> b4df6848e734806f6c31f044089e12d47a9c411c */}
               <h1 class="name">StoXkeeper</h1>
               <h3 className="tagline">The X factor in your Trade</h3>
+              {/* <p className="head-para">Modern investing tools and data for everyone.</p> */}
               <div> 
-                <NavLink  to="/cardbse" class="explore">Start Today!!</NavLink>
+                <a class="explore" href>Start Today!!</a>
               </div>
             </div>
             {/* <Typo /> */}
+            <div className="heading-anim">
+              <p class="typical"><Typical
+                steps={['Stocks analysis 📈', 800, 'Latest news 📰', 800,'Prediction🔮',800]}
+                loop={Infinity}
+                wrapper="p"
+              /></p>
+            </div>
           </div>
       )
       }
